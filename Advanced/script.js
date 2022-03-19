4d248d0715440660ac892a756ffcdb974f271c7f
XusniddinNarkulov <narkulof@gmail.com>
2022-03-19 19:44:18 +0500

    19 march 2022

---
+++ Advanced/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//Selectings
+const html = document.documentElement;
+const body = document.body;
+const head = document.head;
+console.log();
+
+// const allSection = document.querySelectorAll(`.section`);
+// console.log(allSection);
+// const button = document.getElementsByTagName("button");
+// console.log(typeof button);
+// for (let [key, val] of Object.entries(button)) {
+//   console.log(key);
+// }
+
+//creating
+const box = document.createElement("div");
+console.log(box);
